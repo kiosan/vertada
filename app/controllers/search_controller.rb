f7eaaee08be9a96a01b1115fa018638407e56678
unknown <Administrator@.(none)>
2009-01-22 22:38:00 +0200

    fixed order in tag search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,31 +5,16 @@ class SearchController < ApplicationController
     @tags = Tag.find(@tag_ids)
     
     
-    normal_tags = @tags.collect{|f| f.id}
-    
-    
-    
-    #joins = normal_tags.collect{|id| "INNER JOIN tag_sharings ts_#{id} ON ts_#{id}.user_id = #{current_user.id} AND ts_#{id}.tag_id = #{id}" }.join(" ")
-    
-    
-    
-    
-    
-    
-    
     joins = @tag_ids.collect{|id| "INNER JOIN idea_tags it_#{id} ON it_#{id}.idea_id = ideas.id AND it_#{id}.tag_id = #{id} 
                                    INNER JOIN tag_sharings AS ts_#{id} ON ts_#{id}.user_id = #{current_user.id} AND ts_#{id}.tag_id = #{id}" }.join(" ")
     
-    joins << "  "
-    
     select = "ideas.*"
 
-    @ideas = Idea.find(:all, :select=>select, :joins=>joins, :include=>[:idea_tags], :group=>'ideas.id')
+    @ideas = Idea.find(:all, :select=>select, :joins=>joins, :include=>[:idea_tags], :group=>'ideas.id', :order=>"ideas.created_at DESC")
     related_tags = []
     @related_tags = []
     @ideas.each{|t| related_tags += t.visible_tags_for(current_user) }
     related_tags.each do |t|
-    
       @related_tags << t.tag unless @tags.include?(t.tag)
     end
     @related_tags.uniq!
