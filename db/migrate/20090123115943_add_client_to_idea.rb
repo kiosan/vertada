cccc4ca8c6218918d9edec9a6d9aa72ad2d7e13b
unknown <Administrator@.(none)>
2009-01-23 14:09:31 +0200

    added def value to client

--- db/migrate/20090123115943_add_client_to_idea.rb
+++ db/migrate/20090123115943_add_client_to_idea.rb
@@ -1,6 +1,6 @@
 class AddClientToIdea < ActiveRecord::Migration
   def self.up
-    add_column :ideas, :client, :string
+    add_column :ideas, :client, :string, :default => "web"
   end
 
   def self.down
