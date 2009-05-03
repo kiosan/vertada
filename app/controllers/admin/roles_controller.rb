e211f9d9aea07e721125e7278a3f9294d6af8b8b
rkyrychuk <ruslan.kyrychuk@gmail.com>
2009-05-03 22:38:23 +0300

    deleted unused plugins

--- app/controllers/admin/roles_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::RolesController < ApplicationController
-  layout 'streamlined'
-  acts_as_streamlined
-  before_filter :login_required
-end
--- spec/factories.rb
+++
@@ -1,7 +0,0 @@
-require 'factory_girl'
-
-# This will guess the User class
-Factory.define :user do |u|
-  u.login 'John'
-  u.email 'joe@example.com'
-end
